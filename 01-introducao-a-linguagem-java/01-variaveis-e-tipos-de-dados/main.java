9a6f1b63e994ed730cb980e03c0198c6ad9780e6
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2025-02-24 18:01:16 +0000

    24-02

---
+++ 01-introducao-a-linguagem-java/01-variaveis-e-tipos-de-dados/main.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+
+        // Tipos de variáveis
+        int numero;
+        float saldo;
+        String nome;
+
+        System.out.println("Hello world!");
+    }
+}
---
+++ 01-introducao-a-linguagem-java/02-classe-string/main.java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String[] args) {
+
+        // OPERADORES STRINGS
+        // Obtendo o comprimento -> length()
+        // Juntando duas string -> concat()
+        // Comparando duas strings -> equals()
+        // Extraindo substring -> substring()
+        // Verificando se uma string está em outra string -> contains()
+        // Substituir string -> replace()
+
+        String valor = "Estudando Java";
+
+        System.out.println(valor.contains("Java"));
+        System.out.println(valor.length());
+
+    }
+}
