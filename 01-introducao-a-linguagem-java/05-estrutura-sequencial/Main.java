ca38712284c5f98bb5c6d12ac96daf5f48717c88
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2025-02-27 11:44:26 +0000

    27-02

---
+++ 01-introducao-a-linguagem-java/05-estrutura-sequencial/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Executa linha por linha
+
+        double base, altura, area;
+
+        base = 10;
+        altura = 8;
+
+        area = (base * altura) / 2;
+
+        System.err.println("O cálculo do triângulo dá: " + area);
+    }
+}
